567feba955e78280e32905eefc98fdca5f595b9e
Daniel E. Obasi <danielobasi@gmail.com>
2021-07-11 21:26:26 +0100

    Init

--- app/Http/Controllers/BookingBookingController.php
+++ app/Http/Controllers/BookingBookingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\BookingBooking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookingBookingController extends Controller
 {
@@ -12,10 +13,27 @@ class BookingBookingController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-        //TO get authenticated user object for auth protected rout use Auth::user()
+        //all variables from request can be got from the $request object
+        //To get authenticated user object for auth protected rout use Auth::user()
+
+        //get all bookings
+        $data['bookings'] = BookingBooking::all();
+
+        //get user bookings
+        //$data['bookings'] = BookingBooking::where('user_id',Auth::user()->getAuthIdentifier())->get();
+
+        //get user bookings in pages of 20 items
+        //$data['bookings'] = BookingBooking::where('user_id',Auth::user()->getAuthIdentifier())->paginate(20);
+
+
+        //uncomment return statement line.
+        //put in the blade file that has the html template from resources/views folder
+        //do not include extension .blade.php
+
+        //return view('bookinglist',$data);
+
     }
 
     /**
@@ -26,6 +44,9 @@ class BookingBookingController extends Controller
     public function create()
     {
         //
+        //uncomment return statement line.
+        //return the view that has the form to make a booking
+        //return view('bookingform',$data);
     }
 
     /**
@@ -36,7 +57,16 @@ class BookingBookingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $booking = new BookingBooking();
+        $booking->user_id = Auth::user()->getAuthIdentifier();
+        //get the form variables from the request
+        $booking->start_datetime = $request->startTime;
+        $booking->end_datetime = $request->endTime;
+        if ($booking->save()){
+            return redirect()->back()->with('message','Booking created');
+            //or return to any route
+            return redirect()->route('nameofroute');
+        }
     }
 
     /**
@@ -58,7 +88,13 @@ class BookingBookingController extends Controller
      */
     public function edit(BookingBooking $bookingBooking)
     {
-        //
+        if ($booking = BookingBooking::where('id',$bookingBooking->id)->where('user_id',Auth::user()->getAuthIdentifier())->first()){
+            //uncomment this line when you have added your template that displays a single booking
+            //remember to update the name
+            //return view('bookingform',$data);
+
+        }
+
     }
 
     /**
@@ -71,6 +107,21 @@ class BookingBookingController extends Controller
     public function update(Request $request, BookingBooking $bookingBooking)
     {
         //
+        if($booking = BookingBooking::where('id',$bookingBooking->id)
+            ->where('user_id',Auth::user()->getAuthIdentifier())->first()){
+
+            $booking = new BookingBooking();
+            $booking->user_id = Auth::user()->getAuthIdentifier();
+            //get the form variables from the request
+            $booking->start_datetime = $request->startTime;
+            $booking->end_datetime = $request->endTime;
+            if ($booking->save()){
+                return redirect()->back()->with('message','Booking updated');
+            }
+
+        }else{
+            //booking not found or not your booking, can't modify
+        }
     }
 
     /**
@@ -81,6 +132,12 @@ class BookingBookingController extends Controller
      */
     public function destroy(BookingBooking $bookingBooking)
     {
-        //
+        if ($booking = BookingBooking::where('id',$bookingBooking->id)->where('user_id',Auth::user()->getAuthIdentifier())->first()){
+            $booking->delete();
+            return redirect()->back()->with('message','Booking deleted');
+
+        }else{
+            //booking not found or not your booking, can't delete
+        }
     }
 }
